a2adbd3eb07769f8775dea36d59b294c26ca5d61
amliuyong <am.liuyong@qq.com>
2021-10-25 14:49:23 +0800

    first commit

---
+++ source/node_modules/aws-cdk/lib/init-templates/v2/lib/typescript/lib/index.template.ts
@@ -0,0 +1,14 @@
+import { Construct } from 'constructs';
+
+export interface %name.PascalCased%Props {
+  // Define construct properties here
+}
+
+export class %name.PascalCased% extends Construct {
+
+  constructor(scope: Construct, id: string, props: %name.PascalCased%Props = {}) {
+    super(scope, id);
+
+    // Define construct contents here
+  }
+}
