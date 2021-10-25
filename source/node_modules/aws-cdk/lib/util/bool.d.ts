71df75f476f1bdacb324f475616d2e02cffa291a
amliuyong <am.liuyong@qq.com>
2021-10-25 15:27:35 +0800

    rm node_modules

--- source/node_modules/aws-cdk/lib/util/bool.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Converts a boolean into a number.
- *
- * @param bool input boolean
- * @returns 1 if bool is true, and 0 if false
- */
-export declare function numberFromBool(bool: boolean): number;
