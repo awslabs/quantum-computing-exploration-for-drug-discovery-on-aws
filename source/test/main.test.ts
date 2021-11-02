93d5335a460cb67aa8330c195879786c9e640cf4
amliuyong <am.liuyong@qq.com>
2021-11-02 17:15:30 +0800

    rm test

--- source/test/main.test.ts
+++ source/test/main.test.ts
@@ -1,11 +1,9 @@
-import '@aws-cdk/assert/jest';
 import { App } from '@aws-cdk/core';
 import { MyStack } from '../src/stack';
+// test('Snapshot', () => {
+//   const app = new App();
+//   const stack = new MyStack(app, 'test');
 
-test('Snapshot', () => {
-  const app = new App();
-  const stack = new MyStack(app, 'test');
-
-  expect(stack).not.toHaveResource('AWS::S3::Bucket');
-  expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
-});
+//   expect(stack).not.toHaveResource('AWS::S3::Bucket');
+//   expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
+// });
