515605fb39d861110411647602c17697e0b35d81
Yong Liu <23968560@qq.com>
2021-11-15 15:15:22 +0800

    d-wave

--- source/src/molecule-unfolding/lambda/DeviceAvailableCheckLambda/handler.py
+++ source/src/molecule-unfolding/lambda/DeviceAvailableCheckLambda/handler.py
@@ -3,7 +3,7 @@ import boto3
 
 def handler(event, context):
     device_arn = event['deviceArn']
-    if 'DW_2000Q_6' in device_arn:
+    if 'd-wave' in device_arn:
         boto3.setup_default_session(region_name='us-west-2')
 
     braket = boto3.client('braket')
