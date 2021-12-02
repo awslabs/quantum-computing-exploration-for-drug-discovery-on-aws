aea0989e4813be17a3db718ae61eaf522e573dbb
Yong Liu <23968560@qq.com>
2021-12-02 18:00:18 +0800

    rm useless files

---
+++ source/src/package_code.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+set -e
+
+AWS_CMD='aws '
+
+if [[ -n $PROFILE ]];then
+   AWS_CMD="aws --profile $PROFILE"
+fi 
+
+rm -rf ../code/ > /dev/null 2>&1
+rm -rf ../code.zip > /dev/null 2>&1
+
+mkdir ../code/
+
+cp -r * ../code/
+
+cd ../code/
+
+zip -r ../code.zip .
+cd ..
+rm -rf ./code/
+
+latest_ver=$(git tag | tail -1)
+
+s3_paths=(
+    s3://amazon-braket-gcr-qc-sol-common/qc/${latest_ver}/code.zip
+    s3://amazon-braket-gcr-qc-sol-common/qc/latest/code.zip
+)
+
+for s3_path in ${s3_paths[@]};do 
+
+    echo "$AWS_CMD s3 cp ./code.zip $s3_path --acl public-read"
+
+    $AWS_CMD s3 cp ./code.zip $s3_path --acl public-read
+
+    echo $s3_path
+done
+rm ./code.zip
+
+
+
+
+
