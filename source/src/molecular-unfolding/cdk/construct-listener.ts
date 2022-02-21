9fdab150f7aa30d2f7ae89447f855aea0f5deb5b
Yong Liu <yonmzn@amazon.com>
2022-02-21 16:48:12 +0800

    ci: add eslint

--- source/src/molecular-unfolding/cdk/construct-listener.ts
+++ source/src/molecular-unfolding/cdk/construct-listener.ts
@@ -1,102 +1,76 @@
-import * as cdk from 'aws-cdk-lib'
 
 import {
-    aws_s3 as s3
-} from 'aws-cdk-lib'
+  aws_s3 as s3,
+  aws_ec2 as ec2,
+  aws_lambda as lambda,
+  aws_events as events,
+  aws_events_targets as targets,
+  Duration,
+} from 'aws-cdk-lib';
 
 import {
-    aws_ec2 as ec2
-} from 'aws-cdk-lib'
-
-
-import {
-    aws_lambda as lambda
-} from 'aws-cdk-lib'
-
+  Construct,
+} from 'constructs';
 
 import {
-    aws_events as events
-} from 'aws-cdk-lib'
-
-
-import {
-    aws_events_targets as targets
-} from 'aws-cdk-lib'
+  ECRRepoNameEnum,
+  ECRImageUtil,
+} from './utils/utils-images';
 
 import {
-    Construct
-} from 'constructs'
-
-import {
-    ECRRepoNameEnum,
-    ECRImageUtil
-} from './utils/utils-images'
-
-import {
-    RoleUtil
-} from './utils/utils-role'
-
-//const s3n = require('@aws-cdk/aws-s3-notifications')
+  RoleUtil,
+} from './utils/utils-role';
 
 interface Props {
-    region: string;
-    account: string;
-    bucket: s3.Bucket;
-    prefix: string;
-    usePreBuildImage: boolean;
-    vpc: ec2.Vpc;
-    lambdaSg: ec2.SecurityGroup;
-    stackName: string
+  region: string;
+  account: string;
+  bucket: s3.Bucket;
+  prefix: string;
+  usePreBuildImage: boolean;
+  vpc: ec2.Vpc;
+  lambdaSg: ec2.SecurityGroup;
+  stackName: string;
 }
 
 export class EventListener extends Construct {
-    private images: ECRImageUtil
-    private roleUtil: RoleUtil
-    private props: Props
+  private images: ECRImageUtil
+  private roleUtil: RoleUtil
+  private props: Props
 
-    constructor(scope: Construct, id: string, props: Props) {
-        super(scope, id);
-        this.props = props
-        this.images = ECRImageUtil.newInstance()
-        this.roleUtil = RoleUtil.newInstance(scope, this.props)
-        this.createEventListener(this.props.vpc, this.props.lambdaSg)
-    }
+  constructor(scope: Construct, id: string, props: Props) {
+    super(scope, id);
+    this.props = props;
+    this.images = ECRImageUtil.newInstance();
+    this.roleUtil = RoleUtil.newInstance(scope, this.props);
+    this.createEventListener(this.props.vpc, this.props.lambdaSg);
+  }
 
-    private createEventListener(vpc: ec2.Vpc, lambdaSg: ec2.SecurityGroup) {
-        const lambdaRole = this.roleUtil.createCallBackLambdaRole("ParseBraketResultLambdaRole")
-        const code = this.images.getECRImage(ECRRepoNameEnum.Lambda_ParseBraketResult) as lambda.DockerImageCode
-        const parseBraketResultLambda = new lambda.DockerImageFunction(this, 'ParseBraketResultLambda', {
-            code,
-            memorySize: 2048,
-            timeout: cdk.Duration.seconds(900),
-            vpc,
-            vpcSubnets: vpc.selectSubnets({
-                subnetType: ec2.SubnetType.PRIVATE_WITH_NAT
-            }),
-            role: lambdaRole,
-            reservedConcurrentExecutions: 30,
-            securityGroups: [lambdaSg]
-        });
+  private createEventListener(vpc: ec2.Vpc, lambdaSg: ec2.SecurityGroup) {
+    const lambdaRole = this.roleUtil.createCallBackLambdaRole('ParseBraketResultLambdaRole');
+    const code = this.images.getECRImage(ECRRepoNameEnum.Lambda_ParseBraketResult) as lambda.DockerImageCode;
+    const parseBraketResultLambda = new lambda.DockerImageFunction(this, 'ParseBraketResultLambda', {
+      code,
+      memorySize: 2048,
+      timeout: Duration.seconds(900),
+      vpc,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+      }),
+      role: lambdaRole,
+      reservedConcurrentExecutions: 30,
+      securityGroups: [lambdaSg],
+    });
 
-        const rule = new events.Rule(this, 'braketEventrule', {
-            eventPattern: {
-                source: ["aws.braket"],
-                detailType: ["Braket Task State Change"]
-            },
-        });
+    const rule = new events.Rule(this, 'braketEventrule', {
+      eventPattern: {
+        source: ['aws.braket'],
+        detailType: ['Braket Task State Change'],
+      },
+    });
 
-        rule.addTarget(new targets.LambdaFunction(parseBraketResultLambda, {
-            maxEventAge: cdk.Duration.hours(2),
-            retryAttempts: 3,
-        }));
-
-        // this.props.bucket.addEventNotification(s3.EventType.OBJECT_CREATED,
-        //     new s3n.LambdaDestination(parseBraketResultLambda), {
-        //         prefix: `${this.props.prefix}/qc_task_output/`,
-        //         suffix: 'results.json'
-        //     });
-    }
-
-
-
+    rule.addTarget(new targets.LambdaFunction(parseBraketResultLambda, {
+      maxEventAge: Duration.hours(2),
+      retryAttempts: 3,
+    }));
+  }
 }
--- source/src/molecular-unfolding/cdk/utils/utils-images.ts
+++ source/src/molecular-unfolding/cdk/utils/utils-images.ts
@@ -1,69 +1,64 @@
+import * as path from 'path';
 import {
-    aws_ecs as ecs
-} from 'aws-cdk-lib'
-
-import {
-    aws_lambda as lambda
-} from 'aws-cdk-lib'
-
-
-import * as path from 'path'
+  aws_ecs as ecs,
+  aws_lambda as lambda,
+} from 'aws-cdk-lib';
 
 export enum ECRRepoNameEnum {
-    Batch_Create_Model,
-    Batch_Sa_Optimizer,
-    Batch_Qa_Optimizer,
-    Lambda_CheckDevice,
-    Lambda_ParseBraketResult
+  Batch_Create_Model,
+  Batch_Sa_Optimizer,
+  Batch_Qa_Optimizer,
+  Lambda_CheckDevice,
+  Lambda_ParseBraketResult
 };
 
 
 export class ECRImageUtil {
 
-    private constructor() {
+  public static newInstance() {
+    return new this();
+  }
+
+  private constructor() {
+  }
+
+  public getECRImage(name: ECRRepoNameEnum): ecs.ContainerImage | lambda.DockerImageCode {
+
+    if (name == ECRRepoNameEnum.Batch_Create_Model) {
+      return ecs.ContainerImage.fromAsset(
+        path.join(__dirname, '../../'), {
+          file: 'batch-images/create-model/Dockerfile',
+        });
     }
-    
-    public static newInstance() {
-        return new this();
+
+    if (name == ECRRepoNameEnum.Batch_Sa_Optimizer) {
+      return ecs.ContainerImage.fromAsset(
+        path.join(__dirname, '../../'), {
+          file: 'batch-images/sa-optimizer/Dockerfile',
+        });
     }
 
-    public getECRImage(name: ECRRepoNameEnum): ecs.ContainerImage | lambda.DockerImageCode {
-
-        if (name == ECRRepoNameEnum.Batch_Create_Model) {
-            return ecs.ContainerImage.fromAsset(
-                path.join(__dirname, '../../'), {
-                    file: 'batch-images/create-model/Dockerfile',
-                })
-        }
-
-        if (name == ECRRepoNameEnum.Batch_Sa_Optimizer) {
-            return ecs.ContainerImage.fromAsset(
-                path.join(__dirname, '../../'), {
-                    file: 'batch-images/sa-optimizer/Dockerfile',
-                })
-        }
-
-        if (name == ECRRepoNameEnum.Batch_Qa_Optimizer) {
-            return ecs.ContainerImage.fromAsset(
-                path.join(__dirname, '../../'), {
-                    file: 'batch-images/qa-optimizer/Dockerfile',
-                })
-        }
-
-        if (name == ECRRepoNameEnum.Lambda_CheckDevice) {
-            return lambda.DockerImageCode.fromImageAsset(
-                path.join(__dirname, '../../'), {
-                    file: 'lambda/DeviceAvailableCheckLambda/Dockerfile',
-                })
-        }
-
-        if (name == ECRRepoNameEnum.Lambda_ParseBraketResult) {
-            return lambda.DockerImageCode.fromImageAsset(
-                path.join(__dirname, '../../'), {
-                    file: 'lambda/ParseBraketResultLambda/Dockerfile',
-                });
-        }
-
-        throw new Error("Cannot find ecr: " + name);
+    if (name == ECRRepoNameEnum.Batch_Qa_Optimizer) {
+      return ecs.ContainerImage.fromAsset(
+        path.join(__dirname, '../../'), {
+          file: 'batch-images/qa-optimizer/Dockerfile',
+        });
     }
+
+    if (name == ECRRepoNameEnum.Lambda_CheckDevice) {
+      return lambda.DockerImageCode.fromImageAsset(
+        path.join(__dirname, '../../'), {
+          file: 'lambda/DeviceAvailableCheckLambda/Dockerfile',
+        });
+    }
+
+    if (name == ECRRepoNameEnum.Lambda_ParseBraketResult) {
+      return lambda.DockerImageCode.fromImageAsset(
+        path.join(__dirname, '../../'), {
+          file: 'lambda/ParseBraketResultLambda/Dockerfile',
+        });
+    }
+
+    throw new Error('Cannot find ecr: ' + name);
+  }
 }
