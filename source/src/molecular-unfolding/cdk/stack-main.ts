1283310ca91fd4ac21c4ee525d73782e8c63e22a
Yong Liu <yonmzn@amazon.com>
2022-03-07 09:24:35 +0800

    change sol id to SO8027

--- source/src/molecular-unfolding/cdk/stack-main.ts
+++ source/src/molecular-unfolding/cdk/stack-main.ts
@@ -47,7 +47,7 @@ export class MainStack extends SolutionStack {
 
   // constructor
   constructor(scope: Construct, id: string, props: StackProps = {}) {
-    const DESCRIPTION = '(SO8033) Quantum Ready Solution For Drug Discovery';
+    const DESCRIPTION = '(SO8027) Quantum Ready Solution For Drug Discovery';
     super(scope, id, props);
     this.setDescription(DESCRIPTION);
     const stackName = this.stackName.replace(/[^a-zA-Z0-9_]+/, '').toLocaleLowerCase();
