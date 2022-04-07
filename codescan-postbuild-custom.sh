74e51d419883dd085ce9845d3271b95a542d206f
Yong Liu <yonmzn@amazon.com>
2022-04-07 10:21:45 +0800

    add codescan-postbuild-custom.sh

---
+++ codescan-postbuild-custom.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#--------------------------------------------------------------------
+# Usage: this script must exit with a non-zero return code if the 
+# Viperlight scan fails.
+#--------------------------------------------------------------------
+
+pwd 
+
+echo "./codescan-postbuild-custom.sh"
+
+echo "Done codescan-postbuild-custom.sh"
