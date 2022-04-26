a68b602e5360f66b378ff3a2596714125f1efb3d
Yong Liu <yonmzn@amazon.com>
2022-04-26 14:16:17 +0800

    QCEDD

--- source/src/molecular-unfolding/cdk/utils/custom-resource.ts
+++ source/src/molecular-unfolding/cdk/utils/custom-resource.ts
@@ -75,7 +75,7 @@ function addPolicyToCustomResourceLambdaRole(scope: Construct, role: iam.Role): 
       Arn.format({
         service: 'cloudformation',
         region: 'us-west-2',
-        resource: `stack/QRSFDD-BraketEventTo${region}/*`,
+        resource: `stack/QCEDD-BraketEventTo${region}/*`,
         account: account_id,
         arnFormat: ArnFormat.COLON_RESOURCE_NAME,
       }, Stack.of(scope)),
