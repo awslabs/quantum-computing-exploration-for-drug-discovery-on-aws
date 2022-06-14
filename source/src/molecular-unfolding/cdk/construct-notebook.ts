bdda8551550c9189938f3e0208fe59c9ab9a765f
Yong Liu <am.liuyong@qq.com>
2022-06-14 02:46:49 +0000

    use s3_assets

--- source/src/molecular-unfolding/cdk/construct-notebook.ts
+++ source/src/molecular-unfolding/cdk/construct-notebook.ts
@@ -19,10 +19,15 @@ import {
   readFileSync,
 } from 'fs';
 
+import * as path from 'path';
+
 import {
   aws_s3 as s3,
   aws_kms as kms,
   aws_ec2 as ec2,
+  aws_s3_assets as s3_assets,
+  Fn,
+  DockerImage,
 } from 'aws-cdk-lib';
 
 import {
@@ -35,6 +40,7 @@ import {
   Construct,
 } from 'constructs';
 
+import * as Mustache from 'mustache';
 import {
   RoleUtil,
 } from './utils/utils-role';
@@ -62,22 +68,36 @@ export class Notebook extends Construct {
     this.props = props;
     const INSTANCE_TYPE = 'ml.c5.xlarge';
 
+    const srcCodeAsset = new s3_assets.Asset(this, 'srcCodeAsset', {
+      path: path.join(__dirname, '../../../'),
+      bundling: {
+        image: DockerImage.fromRegistry('alpine'),
+        command: ['sh', '-c', `
+            mkdir  /asset-output/source
+            cp -r /asset-input/src /asset-output/source/
+            cp -r /asset-input/test /asset-output/source/
+            cp /asset-input/*.json /asset-output/source/
+            cp /asset-input/*.sh /asset-output/source/
+            cp /asset-input/*.md /asset-output/source/
+        `],
+      },
+    });
+
     this.roleUtil = RoleUtil.newInstance(this, props);
-
-    const githubRepo = 'https://github.com/awslabs/quantum-computing-exploration-for-drug-discovery-on-aws';
-
-    const defaultCodeRepository = this.node.tryGetContext('default_code_repository') || githubRepo;
 
     const notebookRole = this.roleUtil.createNotebookIamRole();
 
+    srcCodeAsset.bucket.grantRead(notebookRole);
+
     let onStartContent = readFileSync(`${__dirname}/resources/onStart.template`, 'utf-8');
 
-    const base64Encode = (str: string): string => Buffer.from(str, 'binary').toString('base64');
-    const onStartContentBase64 = base64Encode(onStartContent);
+    const rawOnStartContent = Mustache.render(onStartContent, {
+      s3_code_path: srcCodeAsset.s3ObjectUrl,
+    });
 
     const installBraketSdk = new CfnNotebookInstanceLifecycleConfig(this, 'install-braket-sdk', {
       onStart: [{
-        content: onStartContentBase64,
+        content: Fn.base64(ec2.UserData.custom(rawOnStartContent).render()),
       }],
     });
 
@@ -85,37 +105,20 @@ export class Notebook extends Construct {
       enableKeyRotation: true,
     });
 
-    let notebookInstance = null;
-    if (defaultCodeRepository.startsWith('https://')) {
-      notebookInstance = new CfnNotebookInstance(this, 'Notebook', {
-        instanceType: INSTANCE_TYPE,
-        roleArn: notebookRole.roleArn,
-        rootAccess: 'Enabled', // Lifecycle configurations need root access to be able to set up a notebook instance
-        lifecycleConfigName: installBraketSdk.attrNotebookInstanceLifecycleConfigName,
-        volumeSizeInGb: 50,
-        kmsKeyId: qcNotebookKey.keyId,
-        securityGroupIds: [this.props.notebookSg.securityGroupId],
-        subnetId: this.props.vpc.selectSubnets({
-          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
-        }).subnetIds[0],
-        directInternetAccess: 'Disabled',
-        defaultCodeRepository: defaultCodeRepository,
-      });
-    } else {
-      notebookInstance = new CfnNotebookInstance(this, 'Notebook', {
-        instanceType: INSTANCE_TYPE,
-        roleArn: notebookRole.roleArn,
-        rootAccess: 'Enabled',
-        lifecycleConfigName: installBraketSdk.attrNotebookInstanceLifecycleConfigName,
-        volumeSizeInGb: 50,
-        kmsKeyId: qcNotebookKey.keyId,
-        securityGroupIds: [this.props.notebookSg.securityGroupId],
-        subnetId: this.props.vpc.selectSubnets({
-          subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
-        }).subnetIds[0],
-        directInternetAccess: 'Disabled',
-      });
-    }
+    const notebookInstance = new CfnNotebookInstance(this, 'Notebook', {
+      instanceType: INSTANCE_TYPE,
+      roleArn: notebookRole.roleArn,
+      rootAccess: 'Enabled',
+      lifecycleConfigName: installBraketSdk.attrNotebookInstanceLifecycleConfigName,
+      volumeSizeInGb: 50,
+      kmsKeyId: qcNotebookKey.keyId,
+      securityGroupIds: [this.props.notebookSg.securityGroupId],
+      subnetId: this.props.vpc.selectSubnets({
+        subnetType: ec2.SubnetType.PRIVATE_WITH_NAT,
+      }).subnetIds[0],
+      directInternetAccess: 'Disabled',
+    });
+
     this.notebookUrl = `https://console.aws.amazon.com/sagemaker/home?region=${this.props.region}#/notebook-instances/openNotebook/${notebookInstance.attrNotebookInstanceName}?view=classic`;
   }
 
--- source/src/molecular-unfolding/cdk/utils/utils-role.ts
+++ source/src/molecular-unfolding/cdk/utils/utils-role.ts
@@ -119,6 +119,17 @@ export class RoleUtil {
 
     role.addToPolicy(new iam.PolicyStatement({
       resources: [
+        'arn:aws:s3:::aws-gcr-solutions/quantum-computing-exploration-for-drug-discovery-on-aws/*',
+        'arn:aws:s3:::solutions-reference/quantum-computing-exploration-for-drug-discovery-on-aws/*',
+        'arn:aws:s3:::solutions-test-reference/quantum-computing-exploration-for-drug-discovery-on-aws/*',
+      ],
+      actions: [
+        's3:GetObject',
+      ],
+    }));
+
+    role.addToPolicy(new iam.PolicyStatement({
+      resources: [
         `arn:aws:s3:::${this.props.bucket.bucketName}`,
       ],
       actions: [
