b7632c88a85cfc09ec39b3f73f60283e4587823a
cuihubin <530051970@qq.com>
2023-01-13 16:37:48 +0800

    feat:update roles

--- source/src/cdk/utils/utils-role.ts
+++ source/src/cdk/utils/utils-role.ts
@@ -17,7 +17,6 @@ limitations under the License.
 
 import {
   aws_iam as iam,
-  // aws_s3 as s3,
 } from 'aws-cdk-lib';
 
 import {
@@ -27,7 +26,6 @@ import {
 interface Props {
   region: string;
   account: string;
-  // bucket: s3.Bucket;
   prefix: string;
   stackName: string;
 }
@@ -44,33 +42,6 @@ export class RoleUtil {
     this.props = props;
     this.scope = scope;
   }
-
-
-  // private addLambdaCommonPolicy(role: iam.Role) {
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     effect: iam.Effect.ALLOW,
-  //     resources: [
-  //       '*',
-  //     ],
-  //     actions: [
-  //       'ec2:CreateNetworkInterface',
-  //       'ec2:DescribeNetworkInterfaces',
-  //       'ec2:DeleteNetworkInterface',
-  //       'ec2:AssignPrivateIpAddresses',
-  //       'ec2:UnassignPrivateIpAddresses',
-  //     ],
-  //   }));
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:logs:*:${this.props.account}:log-group:*`,
-  //     ],
-  //     actions: [
-  //       'logs:CreateLogStream',
-  //       'logs:PutLogEvents',
-  //       'logs:CreateLogGroup',
-  //     ],
-  //   }));
-  // }
 
   public createNotebookIamRole(): iam.Role {
     const role = new iam.Role(this.scope, 'NotebookRole', {
@@ -105,52 +76,19 @@ export class RoleUtil {
       ],
     }));
 
-    // role.addToPolicy(new iam.PolicyStatement({
-    //   resources: [
-    //     `arn:aws:s3:::${this.props.bucket.bucketName}/*`,
-    //     'arn:aws:s3:::braket-*/*',
-    //     'arn:aws:s3:::amazon-braket-*/*',
-    //   ],
-    //   actions: [
-    //     's3:CreateBucket',
-    //     's3:PutObject',
-    //     's3:GetObject',
-    //   ],
-    // }));
-
     role.addToPolicy(new iam.PolicyStatement({
       resources: [
         'arn:aws:s3:::braket-*/*',
         'arn:aws:s3:::amazon-braket-*/*',
+        "arn:aws:s3:::braketnotebookcdk-**"
       ],
       actions: [
-        's3:CreateBucket',
         's3:PutObject',
         's3:GetObject',
-        's3:List*',
+        's3:ListBucket',
       ],
     }));
 
-    // role.addToPolicy(new iam.PolicyStatement({
-    //   resources: [
-    //     `arn:aws:s3:::${this.props.bucket.bucketName}`,
-    //   ],
-    //   actions: [
-    //     's3:ListBucket',
-    //   ],
-    // }));
-
-    role.addToPolicy(new iam.PolicyStatement({
-      resources: [
-        'arn:aws:s3:::cdk-*',
-      ],
-      actions: [
-        's3:CreateBucket',
-        's3:GetObject*',
-        's3:GetBucket*',
-        's3:List*',
-      ],
-    }));
 
     role.addToPolicy(new iam.PolicyStatement({
       resources: [
@@ -177,333 +115,11 @@ export class RoleUtil {
       ],
       actions: [
         'logs:CreateLogStream',
+        "logs:DescribeLogStreams",
         'logs:PutLogEvents',
         'logs:CreateLogGroup',
       ],
     }));
     return role;
   }
-
-
-  // public createBatchJobExecutionRole(roleName: string): iam.Role {
-  //   const ecrAccount = process.env.SOLUTION_ECR_ACCOUNT || '';
-  //   const role = new iam.Role(this.scope, `${roleName}`, {
-  //     assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-  //   });
-
-  //   const resources = [
-  //     `arn:aws:ecr:${this.props.region}:${this.props.account}:repository/*`,
-  //   ];
-
-  //   if (ecrAccount) {
-  //     resources.push(`arn:aws:ecr:${this.props.region}:${ecrAccount}:repository/*`);
-  //   }
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources,
-  //     actions: [
-  //       'ecr:BatchCheckLayerAvailability',
-  //       'ecr:GetDownloadUrlForLayer',
-  //       'ecr:BatchGetImage',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       '*',
-  //     ],
-  //     actions: [
-  //       'ecr:GetAuthorizationToken',
-
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:logs:*:${this.props.account}:log-group:/aws/batch/*`,
-  //     ],
-  //     actions: [
-  //       'logs:CreateLogStream',
-  //       'logs:PutLogEvents',
-  //       'logs:CreateLogGroup',
-  //     ],
-  //   }));
-  //   return role;
-  // }
-
-  // public createCCBatchJobRole(roleName: string): iam.Role {
-  //   const role = new iam.Role(this.scope, `${roleName}`, {
-  //     assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-  //   });
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}/*`,
-  //     ],
-  //     actions: [
-  //       's3:PutObject',
-  //       's3:GetObject',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       'arn:aws:s3:::braket-*/*',
-  //       'arn:aws:s3:::amazon-braket-*/*',
-  //     ],
-  //     actions: [
-  //       's3:GetObject',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}`,
-  //     ],
-  //     actions: [
-  //       's3:ListBucket',
-  //     ],
-  //   }));
-  //   return role;
-  // }
-
-
-  // public createQCBatchJobRole(roleName: string): iam.Role {
-  //   const role = new iam.Role(this.scope, `${roleName}`, {
-  //     assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-  //   });
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}/*`,
-  //     ],
-  //     actions: [
-  //       's3:PutObject',
-  //       's3:GetObject',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:braket:*:${this.props.account}:quantum-task/*`,
-  //     ],
-  //     actions: [
-  //       'braket:GetQuantumTask',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       '*',
-  //     ],
-  //     actions: [
-  //       'braket:GetDevice',
-  //       'braket:CreateQuantumTask',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}`,
-  //     ],
-  //     actions: [
-  //       's3:ListBucket',
-  //     ],
-  //   }));
-  //   return role;
-  // }
-
-  // public createCreateModelBatchJobRole(roleName: string): iam.Role {
-  //   const role = new iam.Role(this.scope, `${roleName}`, {
-  //     assumedBy: new iam.ServicePrincipal('ecs-tasks.amazonaws.com'),
-  //   });
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}/*`,
-  //     ],
-  //     actions: [
-  //       's3:PutObject',
-  //       's3:GetObject',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       'arn:aws:s3:::braket-*/*',
-  //       'arn:aws:s3:::amazon-braket-*/*',
-  //     ],
-  //     actions: [
-  //       's3:GetObject',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}`,
-  //     ],
-  //     actions: [
-  //       's3:ListBucket',
-  //     ],
-  //   }));
-  //   return role;
-  // }
-
-  // public createAggResultLambdaRole(): iam.Role {
-  //   const role = new iam.Role(this.scope, 'AggResultLambdaRole', {
-  //     assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-  //   });
-  //   const table_name1 = `${this.props.stackName}_qc_batch_evaluation_metrics_hist`;
-  //   const table_name2 = `${this.props.stackName}_qc_batch_evaluation_metrics`;
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:athena:*:${this.props.account}:workgroup/primary`,
-  //       `arn:aws:athena:*:${this.props.account}:datacatalog/AwsDataCatalog`,
-  //       `arn:aws:glue:*:${this.props.account}:database/qc_db`,
-  //       `arn:aws:glue:*:${this.props.account}:table/qc_db/${table_name1}`,
-  //       `arn:aws:glue:*:${this.props.account}:table/qc_db/${table_name2}`,
-  //       `arn:aws:glue:*:${this.props.account}:catalog`,
-  //     ],
-  //     actions: [
-  //       'athena:StartQueryExecution',
-  //       'athena:GetQueryExecution',
-  //       'athena:GetQueryResults',
-  //       'glue:UpdateDatabase',
-  //       'glue:DeleteDatabase',
-  //       'glue:CreateDatabase',
-  //       'glue:GetTable',
-  //       'glue:DeleteTable',
-  //       'glue:CreateTable',
-  //       'glue:UpdateTable',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       '*',
-  //     ],
-  //     actions: [
-  //       'athena:ListDataCatalogs',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}/*`,
-  //     ],
-  //     actions: [
-  //       's3:GetObject',
-  //       's3:PutObject',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}`,
-  //     ],
-  //     actions: [
-  //       's3:ListBucket',
-  //       's3:GetBucketLocation',
-  //     ],
-  //   }));
-
-  //   this.addLambdaCommonPolicy(role);
-  //   return role;
-  // }
-
-  // public createWaitForTokenLambdaRole(roleName: string): iam.Role {
-  //   const role = new iam.Role(this.scope, `${roleName}`, {
-  //     assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-  //   });
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}/*`,
-  //     ],
-  //     actions: [
-  //       's3:GetObject',
-  //       's3:PutObject',
-  //     ],
-  //   }));
-
-  //   this.addLambdaCommonPolicy(role);
-
-  //   return role;
-  // }
-
-  // public createCheckQCDeviceLambdaRole(roleName: string): iam.Role {
-  //   const role = new iam.Role(this.scope, `${roleName}`, {
-  //     assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-  //   });
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       '*',
-  //     ],
-  //     actions: [
-  //       'braket:GetDevice',
-  //     ],
-  //   }));
-
-  //   this.addLambdaCommonPolicy(role);
-
-  //   return role;
-  // }
-
-  // public createTaskParametersLambdaRole(roleName: string): iam.Role {
-  //   const role = new iam.Role(this.scope, `${roleName}`, {
-  //     assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-  //   });
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}/*`,
-  //     ],
-  //     actions: [
-  //       's3:GetObject',
-  //       's3:PutObject',
-  //     ],
-  //   }));
-
-  //   this.addLambdaCommonPolicy(role);
-
-  //   return role;
-  // }
-
-
-  // public createCallBackLambdaRole(roleName: string): iam.Role {
-  //   const role = new iam.Role(this.scope, `${roleName}`, {
-  //     assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
-  //   });
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       `arn:aws:s3:::${this.props.bucket.bucketName}/*`,
-  //     ],
-  //     actions: [
-  //       's3:PutObject',
-  //       's3:GetObject',
-  //     ],
-  //   }));
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     resources: [
-  //       'arn:aws:s3:::braket-*/*',
-  //       'arn:aws:s3:::amazon-braket-*/*',
-  //     ],
-  //     actions: [
-  //       's3:GetObject',
-  //     ],
-  //   }));
-
-  //   this.addLambdaCommonPolicy(role);
-
-  //   role.addToPolicy(new iam.PolicyStatement({
-  //     actions: [
-  //       'states:SendTaskSuccess',
-  //       'states:SendTaskFailure',
-  //       'states:SendTaskHeartbeat',
-  //     ],
-  //     resources: [
-  //       '*',
-  //     ],
-  //   }));
-
-  //   return role;
-  // }
 }
