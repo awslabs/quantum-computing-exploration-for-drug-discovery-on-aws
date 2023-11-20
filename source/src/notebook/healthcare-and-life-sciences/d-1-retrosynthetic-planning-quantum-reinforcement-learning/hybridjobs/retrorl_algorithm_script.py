0db81e3e1e5c339d8aa88ff10de49466f8c8f95d
Aoyu <aoyuzhang1989@gmail.com>
2023-11-20 13:33:14 +0800

    feat: reconstruct retro notebook for better experience (#218)
    * fix: retrol classical method
    
    * fix: finish construct for local job
    
    * fix: remove bucket name
    
    * feat: fix band problem

--- source/src/notebook/healthcare-and-life-sciences/d-1-retrosynthetic-planning-quantum-reinforcement-learning/hybridjobs/retrorl_algorithm_script.py
+++ source/src/notebook/healthcare-and-life-sciences/d-1-retrosynthetic-planning-quantum-reinforcement-learning/hybridjobs/retrorl_algorithm_script.py
@@ -7,6 +7,7 @@ import torch.nn.functional as F
 # from deepquantum.gates.qcircuit import Circuit as dqCircuit
 from braket.circuits import Circuit as bkCircuit
 from braket.tracking import Tracker
+from braket.jobs import save_job_checkpoint
 # import deepquantum.gates.qoperator as op
 import boto3
 
@@ -28,7 +29,6 @@ sys.path.append(file_dir)
 
 from utility.RetroGateModel import RetroRLModel
 from utility.RetroRLAgent import RetroRLAgent
-print('here')
 t = Tracker().start()
 
 input_dir = os.environ["AMZN_BRAKET_INPUT_DIR"]
@@ -52,22 +52,21 @@ shots = int(hyperparams["shots"])
 pl_interface = hyperparams["interface"]
 
 model_name = hyperparams["model_name"]
-model_path = hyperparams["model_path"]
+# model_path = hyperparams["model_path"]
 method = hyperparams["method"]
 train_mode = hyperparams["train_mode"]
+episodes = int(hyperparams["episodes"])
 
 # to store files in a list
 list = []
 
 # dirs=directories
-print(input_dir)
-print(device_arn)
-print(os.environ.keys())
-print(file_dir)
-for (root, dirs, file) in os.walk(input_dir):
-    print(f'root {root} dirs {dirs} file {file}')
+# print(input_dir)
+# print(device_arn)
+# print(file_dir)
+# for (root, dirs, file) in os.walk(input_dir):
+#     print(f'root {root} dirs {dirs} file {file}')
 
-input_data_path = f'{input_dir}/input'
 # file1 = np.load(f'{input_data_path}/reactions_dictionary.npy', allow_pickle=True).item()
 # file2 = np.load(f'{input_data_path}/smiles_dictionary.npy', allow_pickle=True).item()
 # file3 = np.load(f'{input_data_path}/target_product.npy').tolist()
@@ -79,32 +78,94 @@ if "copy_checkpoints_from_job" in hyperparams:
 else:
     copy_checkpoints_from_job = None
 
-retro_rl_model = RetroRLModel.load(f'{input_data_path}/{model_path}')
+try:
+    input_data_path = f'{input_dir}/input'
+    os.system.run(f"ls -alh {input_data_path}")
+    # retro_rl_model = RetroRLModel.load(f'{input_data_path}/{model_path}')
+except Exception as e1:
+    try:
+        # Second solution
+        input_data_path = f'{input_dir}/data'
+        os.system.run(f"ls -alh {input_data_path}")
+        # retro_rl_model = RetroRLModel.load(f'{input_data_path}/{model_path}')
+    except Exception as e2:
+        # Handle both solutions failing
+        print(f"Can't find data in {input_dir}/input or {input_dir}/data!!!")
+else:
+    # Code to execute when the first solution succeeds
+    print(f"Found data in {input_data_path}!!")
 
-retro_model = retro_rl_model.get_model(method, model_name)
+
+# retro_model = retro_rl_model.get_model(method, model_name)
 
 agent_param = {}
+# initial the RetroRLModel object
+init_param = {}
+method = ['retro-rl', 'retro-qrl']
+
+for mt in method:
+    if mt == 'retro-rl':
+        init_param[mt] = {}
+        init_param[mt]['param'] = ['inputsize', 'middlesize', 'outputsize']
+    elif mt == 'retro-qrl':
+        init_param[mt] = {}
+        init_param[mt]['param'] = ['n_qubits', 'device', 'framework', 'shots', 'layers']
+    
+# retro_rl_model = RetroRLModel(data=None, method=method, **init_param)
+agent_param['init_param'] = init_param
+
+model_param={}
+method = 'retro-qrl'
+model_param[method] = {}
+model_param[method]['n_qubits'] = [int(model_name.split('_')[0])]
+# model_param[method]['device'] = ['local', 'sv1', 'aspen-m-3', 'aria-2']
+model_param[method]['device'] = [model_name.split('_')[1]]
+model_param[method]['framework'] = [model_name.split('_')[2]]
+# model_param[method]['shots'] = [100,1000]
+model_param[method]['shots'] = [int(model_name.split('_')[3])]
+# model_param[method]['layers'] = [1,2,3]
+model_param[method]['layers'] = [int(model_name.split('_')[4])]
+
+agent_param['model_param'] = model_param
 
 agent_param['data_path'] = input_data_path
 agent_param["train_mode"] = train_mode
 agent_param["model_name"] = model_name
-agent_param["model_path"] = model_path
+# agent_param["model_path"] = model_path
+agent_param["episodes"] = episodes
 
-retro_rl_agent = RetroRLAgent(retro_model, method, **agent_param)
+# retro_rl_agent = RetroRLAgent(retro_model, method, **agent_param)
+retro_rl_agent = RetroRLAgent(True, method, **agent_param)
 
 # retro_rl_agent.game(path=input_data_path)
-retro_rl_agent.game(path=input_data_path)
+retro_rl_agent.game(episodes)
 
 #
-save_path, save_name = retro_rl_agent.save("latest", path=input_data_path)
-if retro_rl_agent.name.split('_')[1] == 'aspen-m2':
-    AWS_REGION = "us-west-1"
-    S3_BUCKET_NAME = "amazon-braket-us-west-1-493904798517"
-    s3_client = boto3.client("s3", region_name=AWS_REGION)
-    s3_client.upload_file(save_path, S3_BUCKET_NAME, f'data/{save_name}')
-else:
-    AWS_REGION = "us-west-1"
-    S3_BUCKET_NAME = "amazon-braket-us-west-1-493904798517"
-    s3_client = boto3.client("s3", region_name=AWS_REGION)
-    s3_client.upload_file(save_path, S3_BUCKET_NAME, f'data/{save_name}')
+save_path, save_name = retro_rl_agent.save(path=input_data_path)
+s3 = os.environ["AMZN_BRAKET_OUT_S3_BUCKET"]
+
+s3_client = boto3.client("s3")
+
+s3_client.upload_file(save_path, s3, f"data/{save_name}")
+
+# os.system(f"aws s3 cp {save_path} s3://{s3}/data/{save_name}")
+# AWS_REGION = "us-west-1"
+# S3_BUCKET_NAME = "amazon-braket-us-west-1-493904798517"
+# s3_client = boto3.client("s3", region_name=AWS_REGION)
+# s3_client.upload_file(save_path, S3_BUCKET_NAME, f'data/{save_name}')
+# job_name = os.environ["AMZN_BRAKET_JOB_NAME"]
+# save_job_checkpoint(
+#     checkpoint_data={"data": f"data for checkpoint from {job_name}"},
+#     checkpoint_file_suffix="checkpoint-1",
+#     )
+# if retro_rl_agent.name.split('_')[1] == 'aspen-m2':
+#     AWS_REGION = "us-west-1"
+#     S3_BUCKET_NAME = "amazon-braket-us-west-1-493904798517"
+#     s3_client = boto3.client("s3", region_name=AWS_REGION)
+#     s3_client.upload_file(save_path, S3_BUCKET_NAME, f'data/{save_name}')
+# else:
+#     AWS_REGION = "us-west-1"
+#     S3_BUCKET_NAME = "amazon-braket-us-west-1-493904798517"
+#     s3_client = boto3.client("s3", region_name=AWS_REGION)
+#     s3_client.upload_file(save_path, S3_BUCKET_NAME, f'data/{save_name}')
 
